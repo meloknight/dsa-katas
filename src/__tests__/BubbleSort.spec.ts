c1394f57c4f26541a3fab7417cfacb2338a801c0
brody <49502879+meloknight@users.noreply.github.com>
2024-06-07 10:19:55 -0700

    Try out a different way to run the tests.

--- src/__tests__/BubbleSort.spec.ts
+++ src/__tests__/BubbleSort.spec.ts
@@ -1,9 +1,20 @@
 import { BubbleSort } from "../katas/BubbleSort";
 
-describe("BubbleSort", () => {
-  it("should sort an array in place", () => {
-    expect(BubbleSort([6, 3, 8, 4, 1111, 234, 0])).toEqual([
-      0, 3, 4, 6, 8, 234, 1111,
-    ]);
-  });
+// describe("BubbleSort", () => {
+//   it("should sort an array in place", () => {
+//     expect(BubbleSort([6, 3, 8, 4, 1111, 234, 0])).toEqual([
+//       0, 3, 4, 6, 8, 234, 1111,
+//     ]);
+//   });
+// });
+
+test("sort an array in place", () => {
+  expect(BubbleSort([6, 3, 8, 4, 1111, 234, 0])).toEqual([
+    0, 3, 4, 6, 8, 234, 1111,
+  ]);
+});
+test("sort an array in place", () => {
+  expect(BubbleSort([66777, 3, 8, 4, 1111, 234, 0])).toEqual([
+    0, 3, 4, 8, 234, 1111, 66777,
+  ]);
 });

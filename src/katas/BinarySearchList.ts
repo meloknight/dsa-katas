14f876356ab759f1efa2c277a9ac8d13170b773d
brody <49502879+meloknight@users.noreply.github.com>
2024-06-12 09:48:47 -0700

    Small modifications to BinarySearchList and QuickSort.

--- src/katas/BinarySearchList.ts
+++ src/katas/BinarySearchList.ts
@@ -1,18 +1,1 @@
-export function BinarySearchList(haystack: number[], needle: number): boolean {
-  let lo = 0;
-  let hi = haystack.length;
-
-  while (lo < hi) {
-    const m = Math.floor(lo + (hi - lo) / 2);
-    const v = haystack[m];
-
-    if (needle === v) {
-      return true;
-    } else if (needle < v) {
-      hi = m;
-    } else {
-      lo = m + 1;
-    }
-  }
-  return false;
-}
+export function BinarySearchList(haystack: number[], needle: number): boolean {}

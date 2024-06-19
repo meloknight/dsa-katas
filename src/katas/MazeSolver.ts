ae5e69c90bf6d92c8b079bce4677467f9710cea2
brody <49502879+meloknight@users.noreply.github.com>
2024-06-19 08:17:04 -0700

    start modifications to MazeSolver kata

--- src/katas/MazeSolver.ts
+++ src/katas/MazeSolver.ts
@@ -1,1 +1,11 @@
-export function MazeSolver() {}
+type Point = {
+  x: number;
+  y: number;
+};
+
+export function MazeSolver(
+  maze: string[],
+  wall: string,
+  start: Point,
+  end: Point
+): Point[] {}
